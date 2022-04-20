f809bf057df3b3bd9aed46215ab3405888018f3e
imarifluqman <arifluqman477@gmail.com>
2022-04-20 06:26:58 +0500

    taha

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ function myFunction() {
     let colorcode = document.getElementById("colorcode").value;
     document.getElementById("hexcode").innerHTML = colorcode;
 }
+
+let r = Math.floor(Math.random() * 100);
+let b = Math.floor(Math.random() * 100);
+let g = Math.floor(Math.random() * 100);
+
+document.body.style.background=r, b, g
+
+console.log(r, b, g);
